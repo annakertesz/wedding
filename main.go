e10518fa68bf1f600cafb27ad701874e8011dc3a
Anna-Kertesz <anna.kertesz@ibm.com>
2019-11-05 22:51:12 +0100

    main and Procfile

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/", func(c *gin.Context) {
+		_, _ = fmt.Fprintln(c.Writer, "wedding")
+	})
+
+	router.Run(":" + port)
+}
